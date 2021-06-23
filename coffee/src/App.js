8443e49456b366d6b303abb5a7a402c55ce69c1b
9terz <9terz666@gmail.com>
2021-06-23 18:48:18 +0700

    archive old app

--- coffee/src/App.js
+++ coffee/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo2.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Under construction
+        </p>
       </header>
     </div>
   );
